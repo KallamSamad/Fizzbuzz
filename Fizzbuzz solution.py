35ecd9a3ee51c299ea15f00390b6d0087686a060
Kallam <kallamsamad@gmail.com>
2025-04-20 12:42:59 +0100

    Add files via upload

---
+++ Fizzbuzz solution.py
@@ -0,0 +1,33 @@
+import random
+def logic(num):
+    if number==3:
+        return "fizz fizz"
+    elif number==36:
+        return "fizz fizz buzz"
+    if num % 3 == 0 and num % 4 == 0:
+        return "fizzbuzz"
+    elif num % 3 == 0 or num ==6 or num==13:
+        return "fizz"
+    elif num % 4 == 0 or num==14:
+        return "buzz"
+    elif num == 36:
+        return "fizz fizz buzz"
+    else:
+        return str(num)
+
+Score = 0
+for x in range(5):
+    number = random.randint(1,100)
+    print(number)
+    answer = input("What do you say?")
+
+    correct_answer = logic(number)
+
+    if answer==correct_answer:
+        print("Correct")
+        Score=Score+1
+    else:
+        print("Incorrect, loser")
+    
+
+print(f"You're score is {Score} out of 5")
